8ba513ff60b14845cf1b22f583bae3fc2a8b6e8a
KunalK123 <70448227+KunalK123@users.noreply.github.com>
2020-10-19 20:08:17 -0400

    Update main.py

--- kunal_all/main.py
+++ kunal_all/main.py
@@ -1,3 +1,21 @@
 #! /usr/bin/env python
 
 print "kunal_all"
+
+from robot_control_class import RobotControl
+import time as t
+
+rc = RobotControl()
+rc.stop_robot()
+
+while(True):
+    d = rc.get_laser(360)
+    
+    if(d < 1.0):
+        #Stop Robot
+        rc.stop_robot()
+        print("Robot Detects Obsticle")
+    else:
+        #Move Forward
+        print("No Obsticle Detetcted")
+
